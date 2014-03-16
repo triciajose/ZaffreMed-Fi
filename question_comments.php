920cf88a82a383e2765dbf3e4674c84de9945081
Wellington Calligaris <wellington.calligaris@hotmail.com>
2014-03-15 18:49:30 -0700

    create page to view comments in a question

---
+++ question_comments.php
@@ -0,0 +1,20 @@
+<!doctype html> 
+<html>
+	<head>
+	  <meta charset="utf-8" />
+	      <title>Discuss by Zaffre</title>
+	      <link rel="stylesheet" type="css" href="main.css">
+	      <link rel="stylesheet" type="css" href="foundation-5.2.1/css/foundation.css">
+	      <link rel="stylesheet" type="css" href="foundation-5.2.1/css/normalize.css">
+
+	      <link href="tabmenu/tabmenu.css" rel="stylesheet" type="text/css" />
+	      <script src="tabmenu/tabmenu.js" type="text/javascript"></script>
+
+	</head>
+	<body>
+		<?php echo $_GET["question"]?>
+
+	</body>
+
+
+</html>
